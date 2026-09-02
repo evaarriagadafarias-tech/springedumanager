f5e646d80cdf4672fbb5278197f51c0a8480f5ea
Eva Arriagada <evaarriagadafarias@gmail.com>
2026-09-02 10:56:51 -0400

    Corrige repositorios y vistas de estudiantes y cursos

--- src/main/java/cl/eva/springedumanager/repository/CursoRepository.java
+++ src/main/java/cl/eva/springedumanager/repository/CursoRepository.java
@@ -1,9 +1,11 @@
 package cl.eva.springedumanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cl.eva.springedumanager.model.Curso;
 
+@Repository
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 
 }
--- src/main/java/cl/eva/springedumanager/repository/EstudianteRepository.java
+++ src/main/java/cl/eva/springedumanager/repository/EstudianteRepository.java
@@ -1,9 +1,11 @@
 package cl.eva.springedumanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cl.eva.springedumanager.model.Estudiante;
 
+@Repository
 public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
 
 }
